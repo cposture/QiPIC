348416ea2779702b3546cac1da2e201ecbfef47c
cposture <cposture@126.com>
2016-09-12 23:20:11 +0800

    download功能正在完成

---
+++ vs2013/QiPic/QiPic/QiniuFile.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Qiniu.Conf;
+using Qiniu.RSF;
+using Qiniu.RS;
+using System.Net;
+using Qiniu.IO;
+using Qiniu.RPC;
+
+
+namespace QiPic
+{
+    class QiniuFile
+    {
+        string m_access_key;
+        string m_secret_key;
+
+        public QiniuFile(string ak, string sk)
+        {
+            m_access_key = ak;
+            m_secret_key = sk;
+        }
+
+        public void Download(string filename, string dir)
+        {
+            
+        }
+    }
+}
