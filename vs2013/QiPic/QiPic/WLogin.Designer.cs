d9e0c67f8f447d447e56cf01e3bc8eaba6d9a5fc
cposture <cposture@126.com>
2016-09-12 22:04:54 +0800

    添加了登录界面

---
+++ vs2013/QiPic/QiPic/WLogin.Designer.cs
@@ -0,0 +1,238 @@
+﻿namespace QiPic
+{
+    partial class WLogin
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(WLogin));
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.AK_Button = new System.Windows.Forms.Button();
+            this.Sk_TextBox = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.Ak_TextBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.Login_Button = new System.Windows.Forms.Button();
+            this.Password_Label = new System.Windows.Forms.Label();
+            this.Username_Label = new System.Windows.Forms.Label();
+            this.Password_Textbox = new System.Windows.Forms.TextBox();
+            this.Username_Textbox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(24, 355);
+            this.label7.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(269, 36);
+            this.label7.TabIndex = 41;
+            this.label7.Text = "获取密钥：\r\n\r\n     七牛官网 - 个人面板 - 个人中心-密钥管理";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(114, 116);
+            this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(95, 12);
+            this.label6.TabIndex = 40;
+            this.label6.Text = "Power By 陈家启";
+            // 
+            // AK_Button
+            // 
+            this.AK_Button.Location = new System.Drawing.Point(183, 219);
+            this.AK_Button.Margin = new System.Windows.Forms.Padding(2);
+            this.AK_Button.Name = "AK_Button";
+            this.AK_Button.Size = new System.Drawing.Size(59, 34);
+            this.AK_Button.TabIndex = 39;
+            this.AK_Button.Text = "注册";
+            this.AK_Button.UseVisualStyleBackColor = true;
+            // 
+            // Sk_TextBox
+            // 
+            this.Sk_TextBox.Location = new System.Drawing.Point(105, 307);
+            this.Sk_TextBox.Margin = new System.Windows.Forms.Padding(2);
+            this.Sk_TextBox.Name = "Sk_TextBox";
+            this.Sk_TextBox.Size = new System.Drawing.Size(138, 21);
+            this.Sk_TextBox.TabIndex = 38;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(32, 311);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(59, 12);
+            this.label5.TabIndex = 37;
+            this.label5.Text = "SecretKey";
+            // 
+            // Ak_TextBox
+            // 
+            this.Ak_TextBox.Location = new System.Drawing.Point(105, 273);
+            this.Ak_TextBox.Margin = new System.Windows.Forms.Padding(2);
+            this.Ak_TextBox.Name = "Ak_TextBox";
+            this.Ak_TextBox.Size = new System.Drawing.Size(138, 21);
+            this.Ak_TextBox.TabIndex = 36;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(32, 277);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(59, 12);
+            this.label3.TabIndex = 35;
+            this.label3.Text = "AccessKey";
+            // 
+            // label4
+            // 
+            this.label4.Image = ((System.Drawing.Image)(resources.GetObject("label4.Image")));
+            this.label4.Location = new System.Drawing.Point(113, 20);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(96, 86);
+            this.label4.TabIndex = 34;
+            // 
+            // Login_Button
+            // 
+            this.Login_Button.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.Login_Button.Location = new System.Drawing.Point(94, 219);
+            this.Login_Button.Margin = new System.Windows.Forms.Padding(2);
+            this.Login_Button.Name = "Login_Button";
+            this.Login_Button.Size = new System.Drawing.Size(59, 34);
+            this.Login_Button.TabIndex = 32;
+            this.Login_Button.Text = "登录";
+            this.Login_Button.UseVisualStyleBackColor = true;
+            // 
+            // Password_Label
+            // 
+            this.Password_Label.AutoSize = true;
+            this.Password_Label.Location = new System.Drawing.Point(32, 177);
+            this.Password_Label.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.Password_Label.Name = "Password_Label";
+            this.Password_Label.Size = new System.Drawing.Size(29, 12);
+            this.Password_Label.TabIndex = 31;
+            this.Password_Label.Text = "密码";
+            // 
+            // Username_Label
+            // 
+            this.Username_Label.AutoSize = true;
+            this.Username_Label.Location = new System.Drawing.Point(32, 143);
+            this.Username_Label.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.Username_Label.Name = "Username_Label";
+            this.Username_Label.Size = new System.Drawing.Size(41, 12);
+            this.Username_Label.TabIndex = 30;
+            this.Username_Label.Text = "用户名";
+            // 
+            // Password_Textbox
+            // 
+            this.Password_Textbox.Location = new System.Drawing.Point(105, 175);
+            this.Password_Textbox.Margin = new System.Windows.Forms.Padding(2);
+            this.Password_Textbox.Name = "Password_Textbox";
+            this.Password_Textbox.PasswordChar = '*';
+            this.Password_Textbox.Size = new System.Drawing.Size(138, 21);
+            this.Password_Textbox.TabIndex = 29;
+            // 
+            // Username_Textbox
+            // 
+            this.Username_Textbox.Location = new System.Drawing.Point(105, 141);
+            this.Username_Textbox.Margin = new System.Windows.Forms.Padding(2);
+            this.Username_Textbox.Name = "Username_Textbox";
+            this.Username_Textbox.Size = new System.Drawing.Size(138, 21);
+            this.Username_Textbox.TabIndex = 28;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.label1.Location = new System.Drawing.Point(256, 147);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 12);
+            this.label1.TabIndex = 42;
+            this.label1.Text = "修改密钥";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.label2.Location = new System.Drawing.Point(252, 278);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 12);
+            this.label2.TabIndex = 43;
+            this.label2.Text = "获取密钥";
+            // 
+            // WLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(321, 406);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.AK_Button);
+            this.Controls.Add(this.Sk_TextBox);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.Ak_TextBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.Login_Button);
+            this.Controls.Add(this.Password_Label);
+            this.Controls.Add(this.Username_Label);
+            this.Controls.Add(this.Password_Textbox);
+            this.Controls.Add(this.Username_Textbox);
+            this.Controls.Add(this.label7);
+            this.Name = "WLogin";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button AK_Button;
+        private System.Windows.Forms.TextBox Sk_TextBox;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox Ak_TextBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button Login_Button;
+        private System.Windows.Forms.Label Password_Label;
+        private System.Windows.Forms.Label Username_Label;
+        private System.Windows.Forms.TextBox Password_Textbox;
+        private System.Windows.Forms.TextBox Username_Textbox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
